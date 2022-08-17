e7174f9c3c8b0b82d563f0065d57d219feafd063
n0bcoder <91626879+n0bcoder@users.noreply.github.com>
2022-08-17 20:03:50 +0700

    Add files via upload

---
+++ esett.py
@@ -0,0 +1,32 @@
+#singleaccount, if you wanna use multiple account add , eg: ['address 1', 'address 2']
+account = ['']
+private = ['']
+#private
+#Defaultgwei
+gwei1 = 2
+#gweiforsell
+gsell = 2
+#gweiforapprove
+gaprv = 2
+#Defaultminbuy
+nonimal1 = 0.05
+#deadblock
+dead1 = 0
+#taxes
+buytax = 24
+selltax = 24
+
+#slippage (Only for max transactions)
+spmx = 140
+
+#gas
+gas = 300000
+
+
+
+#MinLP
+minLP = 1
+
+#node
+nodes = 'https://www.ethercluster.com/etc'
+
